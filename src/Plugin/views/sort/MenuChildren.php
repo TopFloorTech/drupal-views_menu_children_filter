3a3d026bf3dce33a4ef6532afa6aaec60e02df5e
Brian Young <brian@bdyconsulting.com>
2017-04-24 09:31:08 -0400

    Now joining on menu via menu_link_content_data
    No longer preselecting and caching all nodes within a route.
    Now using the menu_link_content_data to get all nodes within a menu.
    Eliminates the need for the helper classes.

--- src/Plugin/views/sort/MenuChildren.php
+++ src/Plugin/views/sort/MenuChildren.php
@@ -3,7 +3,7 @@
 namespace Drupal\views_menu_children_filter\Plugin\views\sort;
 
 use Drupal\views\Plugin\views\sort\SortPluginBase;
-use Drupal\views_menu_children_filter\MenuChildrenHelper;
+use Drupal\views_menu_children_filter\Plugin\views\join\MenuChildrenNodeJoin;
 
 /**
  * Default implementation of the base sort plugin.
@@ -15,41 +15,12 @@ use Drupal\views_menu_children_filter\MenuChildrenHelper;
 class MenuChildren extends SortPluginBase {
 
   public function query() {
-    // Do nothing if this view doesn't use the menu_children_filter argument.
-    if (!empty($this->view->argument['menu_children_filter'])) {
-      $argument = $this->view->argument['menu_children_filter'];
 
-      // Sanity check, although it would be even nicer if we could check against
-      // an interface instead of a specific class.
-      if (!$argument instanceof \Drupal\views_menu_children_filter\Plugin\views\argument\MenuChildren) {
-        return;
-      }
+    MenuChildrenNodeJoin::joinMenuLinksTableToNode($this->query);
+    $tables = $this->query->tables['node_field_data'];
 
-      $targetId = reset($argument->value);
-      $children = MenuChildrenHelper::getChildEntityWeights($targetId, $argument->options['target_menus']);
-
-      // Build an arbitrary sort order statement that is compatible with MySQL,
-      // PostgreSQL and SQLite.
-      $idField = empty($argument->tableAlias) ? 'nid' : "$argument->tableAlias.nid";
-
-      $orderBy = [];
-
-      foreach ($children as $childId => $childWeight) {
-        // Sanitize the id and weight values by casting to int, just to be sure.
-        // We cannot use query placeholders here, because the integers would get
-        // quoted, turn into strings, and break the order by clause when there
-        // are negative weights.
-        $orderBy[] = 'WHEN ' . (int)$childId . ' THEN ' . (int)$childWeight . ' ';
-      }
-
-      if (!empty($orderBy)) {
-        $orderByCase = "CASE $idField " . implode(' ', $orderBy) . ' END';
-
-        $this->query->addField(NULL, $orderByCase, 'menu_children_order');
-        $this->query->addOrderBy(NULL, NULL, $this->options['order'], 'menu_children_order');
-      }
-
-    }
+    $this->query->addOrderBy($tables['menu_link_content_data']['alias'], 'weight', $this->options['order']);
+    $this->query->addOrderBy($tables['node_field_data']['alias'], 'title', $this->options['order']);
+    $this->query->addOrderBy($tables['menu_link_content_data']['alias'], 'id', $this->options['order']);
   }
-
 }
